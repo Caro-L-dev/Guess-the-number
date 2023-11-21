5771056d06a74d0029a89f2238eaeb63fec29ef9
Caro-L-dev <liardcar@gmail.com>
2023-11-21 17:30:10 +0100

    feat: start game interface

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const game = () => {
+  const startContent = document.getElementById("startContent");
+  const mainContent = document.getElementById("mainContent");
 
+  startContent.style.display = "none";
+  mainContent.style.display = "block";
+};
+
+const playBtn = () => {
+  const playBtn = document.getElementById("playBtn");
+  playBtn.addEventListener("click", game);
+};
+
+// playBtn();
+game();
