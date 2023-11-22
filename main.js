6df275c182fb627984b16f69f1c0a404cb067ac0
Caro-L-dev <liardcar@gmail.com>
2023-11-22 11:05:04 +0100

    feat: generate random number

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-const game = () => {
-  const startContent = document.getElementById("startContent");
-  const mainContent = document.getElementById("mainContent");
-
-  startContent.style.display = "none";
-  mainContent.style.display = "block";
-};
-
 const playBtn = () => {
   const playBtn = document.getElementById("playBtn");
   playBtn.addEventListener("click", game);
 };
 
-// playBtn();
+const generateRandomNumberBetween0And500 = () => {
+  const randomNumber = Math.random() * 501;
+  const roundedNumber = Math.round(randomNumber);
+  console.log(roundedNumber);
+  return roundedNumber;
+};
+
+const game = () => {
+  playBtn();
+  const startContent = document.getElementById("startContent");
+  const mainContent = document.getElementById("mainContent");
+
+  startContent.style.display = "none";
+  mainContent.style.display = "block";
+  generateRandomNumberBetween0And500();
+};
+
 game();
