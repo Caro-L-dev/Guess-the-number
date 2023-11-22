060df350571c728af944b21d710b862505608f27
Caro-L-dev <liardcar@gmail.com>
2023-11-22 11:19:38 +0100

    feat: fetch user guess

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// @ts-nocheck
+const displayContent = () => {
+  const startContent = document.getElementById("startContent");
+  const mainContent = document.getElementById("mainContent");
+
+  startContent.style.display = "none";
+  mainContent.style.display = "block";
+};
+
 const playBtn = () => {
   const playBtn = document.getElementById("playBtn");
   playBtn.addEventListener("click", game);
@@ -6,18 +15,27 @@ const playBtn = () => {
 const generateRandomNumberBetween0And500 = () => {
   const randomNumber = Math.random() * 501;
   const roundedNumber = Math.round(randomNumber);
-  console.log(roundedNumber);
+  console.log("Random Number:", roundedNumber);
   return roundedNumber;
+};
+
+const fetchUserGuess = () => {
+  const inputElement = document.getElementById("yourGuess");
+
+  const submitBtn = document.getElementById("submitBtn");
+  submitBtn.addEventListener("click", () => {
+    const userGuess = fetchUserGuess();
+    console.log("User Guess:", userGuess);
+  });
+
+  return inputElement.value;
 };
 
 const game = () => {
   playBtn();
-  const startContent = document.getElementById("startContent");
-  const mainContent = document.getElementById("mainContent");
-
-  startContent.style.display = "none";
-  mainContent.style.display = "block";
+  displayContent();
   generateRandomNumberBetween0And500();
+  fetchUserGuess();
 };
 
 game();
